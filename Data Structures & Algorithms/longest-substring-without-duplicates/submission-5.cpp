c6a2cfad08a9b8f33224681fdaea969a20d59ab4
Chenglong Wei <longprowei@gmail.com>
2026-09-05 17:04:44 +1000

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> charIndexMap;
+
+        int maxLen = 0;
+        int left = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (charIndexMap.contains(s[i])) {
+                left = max(left, charIndexMap[s[i]] + 1);
+                charIndexMap[s[i]] = i; //update to the latest index
+            }
+            charIndexMap[s[i]] = i;
+            maxLen = max(maxLen, i - left + 1);
+        }
+        
+        return maxLen;
+    }
+};
