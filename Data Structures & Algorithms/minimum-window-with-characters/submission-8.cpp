d27a67ea866f946dd22de4a12a5f52a3aeb36b7f
Chenglong Wei <longprowei@gmail.com>
2026-09-07 00:03:42 +1000

    Add: minimum-window-with-characters - submission-8

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-8.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char, int> tFreq;
+        for (char c : t) {
+            tFreq[c]++;
+        }
+
+        string minSubStr;
+        int minL = -1;
+        int minLen = s.size() + 1;
+        unordered_map<char, int> sFreq;
+        int left = 0;
+        int havingCount = 0;
+        int needChars = tFreq.size();
+        for (int i = 0; i < s.size(); i++) {
+            sFreq[s[i]]++;
+            
+            if (tFreq.contains(s[i]) && sFreq[s[i]] == tFreq[s[i]]) {
+                havingCount++;
+            }
+
+            while (havingCount == needChars) {
+                if (i - left + 1 < minLen) {
+                    minL = left;
+                    minLen = i - left + 1;
+                }
+                sFreq[s[left]]--;
+                if (tFreq.contains(s[left]) && sFreq[s[left]] < tFreq[s[left]]) {
+                    havingCount--;
+                }
+                left++;
+            }
+        }
+
+        if (minL != -1) {
+            minSubStr = s.substr(minL, minLen);
+        }
+
+        return minSubStr;
+    }
+};
