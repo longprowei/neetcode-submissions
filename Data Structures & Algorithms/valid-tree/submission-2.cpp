e64afc989ba6ec72cf12b865d45f2eaf42a0ca36
Chenglong Wei <longprowei@gmail.com>
2026-09-16 12:17:13 +1000

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    vector<bool> visited;
+    int visitedCount = 0;
+private:
+    bool hasCycle(vector<vector<int>>& adjLists, int u, int from) {
+        if (visited[u]) {
+            return true;
+        }
+
+        visited[u] = true;
+        visitedCount++;
+        for (int v : adjLists[u]) {
+            if (v != from) {
+                if (hasCycle(adjLists, v, u)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if (edges.size() != n - 1) {
+            return false;
+        }
+
+        vector<vector<int>> adjLists(n);
+        visited.resize(n, false);
+        for (const auto& edge : edges) {
+            adjLists[edge[0]].push_back(edge[1]);
+            adjLists[edge[1]].push_back(edge[0]);
+        }
+        if (hasCycle(adjLists, 0, -1)) {
+            return false;
+        }
+        
+        return visitedCount == n;
+    }
+};
