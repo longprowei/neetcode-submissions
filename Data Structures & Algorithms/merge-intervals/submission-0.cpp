5a73986710b13d99d6754ea888250d7705faa1c6
Chenglong Wei <longprowei@gmail.com>
2026-09-19 17:24:34 +1000

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> res;
+        for (const auto& interval : intervals) {
+            if (res.empty() || res.back()[1] < interval[0]) {
+                res.push_back(interval);
+            } else {
+                res.back()[1] = max(interval[1], res.back()[1]);
+            }
+        }
+
+        return res;
+    }
+};
