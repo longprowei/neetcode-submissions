7843770d42c378d8a81c7f042ab44dfb5a1a5153
Chenglong Wei <longprowei@gmail.com>
2026-09-25 17:09:14 +1000

    Add: sliding-window-maximum - submission-3

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        priority_queue<pair<int, int>> pq;
+        vector<int> res;
+
+        for (int i = 0; i < k; i++) {
+            pq.emplace(nums[i], i);
+        }
+        res.push_back(pq.top().first);
+
+        for (int i = k; i < nums.size(); i++) {
+            while (!pq.empty() && pq.top().second <= i - k) {
+                pq.pop();
+            }
+
+            pq.emplace(nums[i], i);
+            res.push_back(pq.top().first);
+        }
+
+        return res;
+    }
+};
